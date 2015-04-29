175887c0f1bbd9bbd472cc76199d68b3af5a0d8d
liangjingyang <simple.continue@gmail.com>
2015-04-29 21:03:45 +0800

    first commit

---
+++ foodpro_design/data/sitemap.js
@@ -0,0 +1,8 @@
+﻿var sitemap = 
+
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k),_(c,l,e,f,g,m),_(c,n,e,f,g,o),_(c,p,e,f,g,q),_(c,r,e,f,g,s)])]);}; 
+var b="rootNodes",c="pageName",d="欢迎页",e="type",f="Wireframe",g="url",h="欢迎页.html",i="children",j="主页",k="主页.html",l="修改页",m="修改页.html",n="搜索页",o="搜索页.html",p="详细成分页",q="详细成分页.html",r="日历页",s="日历页.html";
+return _creator();
+})();
